5a7d4dde4b941a5cfd267c163eaa894a1d9b3aee
admin <admin@admins-MacBook-Pro.local>
2023-07-17 19:03:31 +0200

    Moved some code to private methods in JsonReader

--- src/main/java/org/example/JsonReader.java
+++ src/main/java/org/example/JsonReader.java
@@ -16,27 +16,40 @@ public class JsonReader {
     private static final String TIMESTAMP_FILTER = "2017-07-01 00:00:00";
     private static final String OUTPUT_FILE_NAME = "filtered_statuses.csv";
 
+
     public JsonReader(String path){
+        List<String[]> dataToWrite = readData(path);
+        writeDataToCsv(dataToWrite);
+    }
+
+    private List<String[]> readData(String path){
         File jsonFile = new File(path);
         try {
             ObjectMapper mapper = new ObjectMapper();
             List<Status> statuses = mapper.readValue(jsonFile, new TypeReference<List<Status>>() {});
+            statuses = filterData(statuses);
 
-            statuses = statuses.stream()
-                    .filter(status -> status.getKontaktTs().after(Timestamp.valueOf(TIMESTAMP_FILTER)))
+            return statuses.stream()
+                    .sorted(new StatusComparator())
+                    .map(Status::toStringArray)
                     .collect(Collectors.toList());
-
-            List<String[]> listData = statuses.stream()
-                                                .sorted(new StatusComparator())
-                                                .map(Status::toStringArray)
-                                                .collect(Collectors.toList());
-
-            try (CSVWriter writer = new CSVWriter(new FileWriter(OUTPUT_FILE_NAME))) {
-                writer.writeAll(listData);
-            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void writeDataToCsv(List<String[]> dataToWrite){
+        try (CSVWriter writer = new CSVWriter(new FileWriter(OUTPUT_FILE_NAME))) {
+            writer.writeAll(dataToWrite);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Status> filterData(List<Status> dataToFilter){
+        return dataToFilter.stream()
+                .filter(status -> status.getKontaktTs().after(Timestamp.valueOf(TIMESTAMP_FILTER)))
+                .collect(Collectors.toList());
     }
 }
